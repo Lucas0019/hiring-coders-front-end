ec24f3c04e813983f39d180e379cf69b7491feca
Lucas Xavier <52923100+Lucas0019@users.noreply.github.com>
2021-06-30 09:26:45 -0300

    feat: :fire: Add class basic js

---
+++ semana-II/js-basico/tipo-dados.js
@@ -0,0 +1,27 @@
+// Tipo de dado pode ser alterado de acordo com a necessidade do Algoritmo
+
+const name = "Lucas" // String
+const age = 21 // Number 
+const height = 1.74 // number
+const isStudent = false // boolean
+
+null
+undefined
+
+// Objeto
+const person = {
+  "name": "Lucas",
+  "age": 21 
+}
+
+// Array
+const frut = ["Pera", "Goiaba", "Uva"]
+
+// function
+function sum(a,b) {
+  return a + b
+}
+
+console.log(sum(1,2)); // 3
+
+// operador typeof - identifica a variavel
---
+++ semana-II/js-basico/variaveis.js
@@ -0,0 +1,12 @@
+// Escopo Global, Escopo de função, Não tem escopo bloqueado, poder ser ressignificado
+// poder ser redeclarado, poder ser hospedado
+var name = 'Lucas' // Sobre efeio de hoisting
+ 
+
+// Não tem escopo global, tem escopo de função, tem escopo bloqueado, poder ser ressignificado
+// não poder redeclarado e não pode ser hospedado
+let language = 'javascript'
+
+// Não tem escopo global, possue apenas escopo da função e escopo bloqueado
+// Não poder ser ressignificado, nem declarado ou hospedado novamente
+const pattern = "ECMAScript"
