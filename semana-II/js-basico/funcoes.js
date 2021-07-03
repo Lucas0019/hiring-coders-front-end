6ae8994d4865167ee02dd443f9364dbf62fbf20f
Lucas Xavier <52923100+Lucas0019@users.noreply.github.com>
2021-07-03 13:43:24 -0300

    feat: Add content funtion js

---
+++ semana-II/js-basico/funcoes.js
@@ -0,0 +1,22 @@
+// Funções do javascript
+
+function sayHello(name)  {
+  console.log("Eae Dev: " + name);
+}
+
+
+const sumNumber = function(a,b) {
+  return  console.log(a + b);
+
+}
+
+listNumber = () => {
+  
+  let number = "11 998221299"
+  console.log(number);
+}
+
+
+sumNumber(1, 2);// 3
+sayHello("Lucas"); // Lucas
+listNumber();
