d469ccaf6b6396d3fcb4e49562b951ad46dc9369
Lucas Xavier <lxavier.00@gmail.com>
2021-07-05 22:14:28 -0300

    feat: :fire: Add class js intermediary

---
+++ semana-II/js-intermediario/template-string.js
@@ -0,0 +1,12 @@
+// Template Strings são strings que permitem expressões embutidas. 
+// Você pode utilizar string multi-linhas e interpolação de string com elas.
+
+`\`` === '`' // --> true
+
+ 
+"Lucas Xavier"
+let profissao = "Front end Developer"
+
+console.log(`A profissão do Lucas é  ${profissao}`)
+
+console.log("Lucas é" + " " + profissao)
