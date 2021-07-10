ab4f53f77de8fb376f70c2730d18271e04f5a668
Lucas Xavier <lxavier.00@gmail.com>
2021-07-10 18:59:20 -0300

    feat: Add axios and request made to github api

--- semana-III/react/gitbook/src/App.js
+++ semana-III/react/gitbook/src/App.js
@@ -1,10 +1,26 @@
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
 
-function App() {
+function App(props) {
+  const [usuario, setUsuario] = useState("");
+
+  function handlePesquisa() {
+    axios
+      .get(`https://api.github.com/users/${usuario}/repos`)
+      .then((response) => console.log(response.data));
+  }
   return (
-    <div className="App">
-      <h1>Git Book</h1>
-    </div>
+    <>
+      <input
+        clasName="usuario__input"
+        placeholder="Usuário"
+        value={usuario}
+        onChange={(e) => setUsuario(e.target.value)}
+      />
+      <button type="button" onClick={handlePesquisa}>
+        Pesquisar
+      </button>
+    </>
   );
 }
 
