6169de2c448ae273aa916a5c1aedaf0f584eee9c
lucas <lxavier.00@outlook.com>
2021-07-14 20:21:53 -0300

    feat: :fire: Implemented Conditional rendering

--- semana-III/react/gitbook/src/Pages/Home/Home.js
+++ semana-III/react/gitbook/src/Pages/Home/Home.js
@@ -1,43 +1,54 @@
 import React, { useState } from "react";
 import axios from "axios";
-import * as S from './styled'
-import { useHistory } from "react-router-dom"
+import * as S from "./styled";
+import { useHistory } from "react-router-dom";
 
 function Home(props) {
-
-  const history = useHistory()
+  const history = useHistory();
   const [usuario, setUsuario] = useState("");
+  const [error, setError] = useState(false);
 
   function handlePesquisa() {
     axios
       .get(`https://api.github.com/users/${usuario}/repos`)
-      .then(response => {
-         const repositories = response.data
-         const repositoriesName = []
-         
-         repositories.map((repository) => {
-             repositoriesName.push(repository.name)
-         })
-         localStorage.setItem('repositoriesName', JSON.stringify(repositoriesName))
-         history.push('/repositories')
+      .then((response) => {
+        const repositories = response.data;
+        const repositoriesName = [];
+
+        repositories.map((repository) => {
+          repositoriesName.push(repository.name);
+        });
+        localStorage.setItem(
+          "repositoriesName",
+          JSON.stringify(repositoriesName)
+        );
+        setError(false);
+        history.push("/repositories");
+      })
+
+      .catch((error) => {
+        setError(true);
       });
   }
 
-
   return (
-    <S.Container>
-      <S.Input
-        clasName="usuario__input"
-        placeholder="Usuário"
-        value={usuario}
-        onChange={(e) => setUsuario(e.target.value)}
-      />
-      <S.Button type="button" onClick={handlePesquisa}>
-        Pesquisar
-      </S.Button>
-    </S.Container>
+    <S.HomeContainer>
+      <S.Content>
+        <S.Input
+          clasName="usuario__input"
+          placeholder="Usuário"
+          value={usuario}
+          onChange={(e) => setUsuario(e.target.value)}
+        />
+        <S.Button type="button" onClick={handlePesquisa}>
+          Pesquisar
+        </S.Button>
+      </S.Content>
+      {error ? (
+        <S.ErrorMsg>Usuário não encontrado, tente uma nova busca</S.ErrorMsg>
+      ) : null}
+    </S.HomeContainer>
   );
 }
 
 export default Home;
-
--- semana-III/react/gitbook/src/Pages/Home/styled.js
+++ semana-III/react/gitbook/src/Pages/Home/styled.js
@@ -1,13 +1,19 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
+export const HomeContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh
+` 
+
+export const Content = styled.div`
   width: 100vw;
-  height: 100vh;
   display: flex;
   align-items: center;
   justify-content: center;
 `
-
 export const Input = styled.input`
   border: 1px solid #ddd;
   height: 1.5rem;
@@ -35,3 +41,13 @@ export const Button = styled.button`
   }
 ` 
 
+export const  ErrorMsg = styled.span`
+  color: tomato;
+  font-family: sans-serif;
+  font-size: .6rem;
+  display: block;
+  margin-top: 1rem;
+  background: #f6dfdf;
+  padding: .5rem;
+  border-radius: .25rem;
+`
