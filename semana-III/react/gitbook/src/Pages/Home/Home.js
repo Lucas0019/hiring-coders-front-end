225538c9ea12a8addd70bf01075c5b551d7d5412
lucas <lxavier.00@outlook.com>
2021-07-14 17:53:10 -0300

    feat: :fire: Rendering repositories in research and declarative programming

--- semana-III/react/gitbook/src/Pages/Home/Home.js
+++ semana-III/react/gitbook/src/Pages/Home/Home.js
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
 import axios from "axios";
 import * as S from './styled'
+import { useHistory } from "react-router-dom"
 
 function Home(props) {
+
+  const history = useHistory()
   const [usuario, setUsuario] = useState("");
 
   function handlePesquisa() {
@@ -16,10 +19,11 @@ function Home(props) {
              repositoriesName.push(repository.name)
          })
          localStorage.setItem('repositoriesName', JSON.stringify(repositoriesName))
+         history.push('/repositories')
       });
   }
 
-  
+
   return (
     <S.Container>
       <S.Input
--- semana-III/react/gitbook/src/Pages/Repositpries/Repositories.js
+++ semana-III/react/gitbook/src/Pages/Repositpries/Repositories.js
@@ -1,24 +1,26 @@
-import React from 'react'
-import * as S from './styled'
+import React, { useState, useEffect } from "react";
+import * as S from "./styled";
 
 const Repositories = () => {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    let repositoriesName = localStorage.getItem("repositoriesName");
+    repositoriesName = JSON.parse(repositoriesName);
+    setRepositories(repositoriesName);
+    localStorage.clear();
+  }, []);
+
   return (
     <S.Container>
-     <S.Title>Repositories</S.Title> 
-     <S.List>
-       <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-       <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-       <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-       <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-       <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-       <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-       <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-       <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-       <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-       <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-     </S.List>
+      <S.Title>Repositories</S.Title>
+      <S.List>
+        {repositories.map((repository) => {
+          return <S.ListItem> Repositório {repository} </S.ListItem>;
+        })}
+      </S.List>
     </S.Container>
-  )
-}
+  );
+};
 
-export default Repositories
+export default Repositories;
