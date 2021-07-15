8c3e1b3e24b49e2d12559caa672f082fdea19937
lucas <lxavier.00@outlook.com>
2021-07-14 23:40:13 -0300

    feat: :fire: home server for testing created

--- semana-IV/graphql/dev-client-server/src/main.js
+++ semana-IV/graphql/dev-client-server/src/main.js
@@ -1,1 +1,33 @@
-console.log('Brabo 🔥');
+import { createServer } from "http";
+
+const server = createServer((req, res) => {
+  switch (req.url) {
+    case "/status": {
+      res.writeHead(200, {
+        "Content-Type": "application/json",
+      });
+      res.write(
+        JSON.stringify({
+          status: "ok",
+          service: {
+            name: "My Service",
+            version: "1.0.0",
+          }
+        })
+      );
+      res.end();
+    }
+
+    default: {
+      res.writeHead(404, "Service not found");
+      res.end();
+    }
+  }
+});
+
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8000;
+const HOSTNAME = process.env.HOSTNAME || "127.0.0.1";
+
+server.listen(PORT, HOSTNAME, () => {
+  console.log(`Server is listening on port http://${HOSTNAME}:${PORT}.🔥`);
+});
