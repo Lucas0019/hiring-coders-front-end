ae952c76d8058d0dfcf96d93a03d10c094f4ba03
lucas <lxavier.00@outlook.com>
2021-07-15 11:30:17 -0300

    feat: :fire: created route and validation for login page

--- semana-IV/graphql/dev-client-server/src/main.js
+++ semana-IV/graphql/dev-client-server/src/main.js
@@ -1,5 +1,7 @@
 import { createServer } from "http";
-
+import { readFile } from "fs";
+import { resolve } from "path"
+ 
 const server = createServer((req, res) => {
   switch (req.url) {
     case "/status": {
@@ -12,10 +14,32 @@ const server = createServer((req, res) => {
           service: {
             name: "My Service",
             version: "1.0.0",
-          }
+          },
         })
       );
       res.end();
+    }
+
+    case "/sign-in": {
+
+      const path = resolve(__dirname, './pages/sign-in.html')
+
+      readFile(path, (error, file) => {
+        if (error) {
+          res.writeHead(500, 'Cant\'t process HTML file') 
+          res.end();
+          return 
+        }
+
+        res.writeHead(200)
+        res.write(file)
+        res.end()
+      });
+      break;
+    }
+
+    case "/authenticate": {
+      break;
     }
 
     default: {
